c0079754d52795e6d372751754615833b5d53821
Bunyodbek <nasriddinoffbunyodbek@gmail.com>
2023-04-29 23:55:57 +0500

    model.js file add

---
+++ utils/model.js
@@ -0,0 +1,30 @@
+const { readFileSync, writeFileSync } = require("fs");
+const { resolve } = require("path");
+const { createHash } = require("crypto");
+
+function read(fileName) {
+  return JSON.parse(readFileSync(resolve("database", fileName + ".json")));
+}
+
+function write(fileName, data) {
+  writeFileSync(
+    resolve("database", fileName + ".json"),
+    JSON.stringify(data, null, 2)
+  );
+  return true;
+}
+
+function hashPasswd(password) {
+  return createHash("sha256").update(password).digest("hex");
+}
+
+function queryFilter(key, value, data) {
+  return filtered = data.filter((item) => item[key] == value);
+}
+
+module.exports = {
+  read,
+  write,
+  hashPasswd,
+  queryFilter
+};
